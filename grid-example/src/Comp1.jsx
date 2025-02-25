d193e76e733b2271d7b30d36ba3406ae2c4e9fd1
koduru-sardhika <kodurusardhika@gmail.com>
2025-02-25 12:53:33 +0530

    MSG

---
+++ grid-example/src/Comp1.jsx
@@ -0,0 +1,17 @@
+import "./Comp1.css";
+const Comp1 =()=>{
+    return(
+        <>
+        <div className="parent">
+            <div className="header">Header</div>
+            <div className="leftmenu">LeftMenu</div>
+            <div className="main">Main</div>
+            <div className="rightmenu">RightMenu</div>
+            <div className="footer">Footer</div>
+            
+        </div>
+        </>
+    )
+
+}
+export default Comp1;
---
+++ grid-example/src/main.jsx
@@ -0,0 +1,9 @@
+// import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+// import App from './App.jsx'
+import Comp1 from './Comp1.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <Comp1></Comp1>
+)
